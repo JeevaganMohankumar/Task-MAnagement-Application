18b8d08f011ef64d653a5d72fdf3294f9de0f420
JeevaganMohankumar <jeevaganmohankumar2@gmail.com>
2026-06-03 10:25:00 +0530

    Create task.js

---
+++ routes/task.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const Task = require("../models/Task");
+const auth = require("../middleware/auth");
+
+router.get("/", auth, async(req,res)=>{
+
+    const tasks = await Task.find({
+        userId:req.user.id
+    });
+
+    res.json(tasks);
+});
+
+router.post("/", auth, async(req,res)=>{
+
+    const task = new Task({
+        title:req.body.title,
+        description:req.body.description,
+        userId:req.user.id
+    });
+
+    await task.save();
+
+    res.json(task);
+});
+
+router.put("/:id", auth, async(req,res)=>{
+
+    const task = await Task.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    );
+
+    res.json(task);
+});
+
+router.delete("/:id", auth, async(req,res)=>{
+
+    await Task.findByIdAndDelete(
+        req.params.id
+    );
+
+    res.json({
+        msg:"Task Deleted"
+    });
+});
+
+module.exports = router;
