fadd98ff7b30b7ba559eed08f3664c9e29446a64
JeevaganMohankumar <jeevaganmohankumar2@gmail.com>
2026-06-03 10:23:08 +0530

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async(req,res)=>{
+
+    const {name,email,password} = req.body;
+
+    const hash = await bcrypt.hash(password,10);
+
+    const user = new User({
+        name,
+        email,
+        password:hash
+    });
+
+    await user.save();
+
+    res.json({
+        msg:"User Registered"
+    });
+});
+
+router.post("/login", async(req,res)=>{
+
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(400).json({
+            msg:"User Not Found"
+        });
+    }
+
+    const match = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if(!match){
+        return res.status(400).json({
+            msg:"Wrong Password"
+        });
+    }
+
+    const token = jwt.sign(
+        {user:{id:user._id}},
+        process.env.JWT_SECRET
+    );
+
+    res.json({token});
+});
+
+module.exports = router;
